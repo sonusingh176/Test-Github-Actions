7587ee40849cfb8911a4dc7a3b6d274a02516460
sonusingh176 <sonusingh82601@gmail.com>
2026-06-07 09:55:53 +0530

    write github action for clone

---
+++ src/App.test.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import {render,screen} from '@testing-library/react'
+import {describe,test,expect} from "vitest"
+import App from "./App"
+
+test('render Github Actions Heading',()=>{
+    render(<App/>);
+    const headingElement = screen.getByText(/Github Actions/i);
+    expect(headingElement).toBeInTheDocument();
+})
+
+test('contains an h1 element',()=>{
+    const {container} =render(<App/>);
+    const h1Element =container.querySelector('h1');
+    expect(h1Element).toBeInTheDocument();
+
+})
+
+test('contains an h2 element',()=>{
+    const {container}=render(<App/>);
+    const h2Element = container.querySelector('h2');
+    expect(h2Element).toBeInTheDocument();
+})
