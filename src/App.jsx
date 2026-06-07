723294e85fbf33e4509fe9df834e38fb60b24e7b
sonusingh176 <sonusingh82601@gmail.com>
2026-06-07 17:53:37 +0530

    add artifact

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
   <h1>Github Actions</h1>
-  <h2>React Testing with Vitest by sonu kaushik</h2>
+  <h2>React Testing with Vitest by sonu kaushik </h2>
     </>
   )
 }
